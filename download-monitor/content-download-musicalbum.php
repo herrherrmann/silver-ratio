a0e8539ec697936fbeef2a16bf87d1fe0fd621db
Sebastian Herrmann <sebastian@herrherrmann.net>
2017-10-31 20:53:08 +0100

    better download album style

--- download-monitor/content-download-musicalbum.php
+++ download-monitor/content-download-musicalbum.php
@@ -1,14 +1,16 @@
 <?php global $dlm_download; ?>
-<div class="download-album">
+<div class="download-musicalbum">
 	<a class="cover" href="<?php $dlm_download->the_download_link(); ?>" title="Download (<?php $dlm_download->the_filesize(); ?>)" rel="nofollow">
 		<?php $dlm_download->the_image('thumbnail'); ?>
 	</a>
 	<div class="meta">
-		<div class="title">
-			<?php $dlm_download->the_title(); ?>
-		</div>
-		<div class="year">
-			<?php $dlm_download->the_short_description(); ?>
+		<div class="meta-info">
+			<div class="title">
+				<?php $dlm_download->the_title(); ?>
+			</div>
+			<div class="year">
+				<?php $dlm_download->the_short_description(); ?>
+			</div>
 		</div>
 		<a class="download-link" href="<?php $dlm_download->the_download_link(); ?>" title="Download (<?php $dlm_download->the_filesize(); ?>)" rel="nofollow">
 			Download
