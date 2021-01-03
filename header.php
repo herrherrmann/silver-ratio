7617782805df64c671577a558ac2e813674aca68
Sebastian Herrmann <sebastian@herrherrmann.net>
2021-01-03 14:45:58 +0100

    Use Roboto for main font-family

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link rel="preconnect" href="https://fonts.gstatic.com">
-	<link href="https://fonts.googleapis.com/css2?family=Cantata+One&family=Josefin+Sans:ital,wght@0,400;0,600;0,700;1,400&display=swap" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css2?family=Cantata+One&family=Roboto:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
     <?php wp_head(); ?>
 </head>
