b93bb03f17974842256e84ff978c271cb3e10c92
Sebastian Herrmann <sebastian@herrherrmann.net>
2021-01-03 13:15:28 +0100

    Load Google fonts in new version with preconnect

--- header.php
+++ header.php
@@ -5,7 +5,8 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<link href="https://fonts.googleapis.com/css?family=Cantata+One|Josefin+Sans:400,600,700" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Cantata+One&family=Josefin+Sans:ital,wght@0,400;0,600;0,700;1,400&display=swap" rel="stylesheet"> 
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
     <?php wp_head(); ?>
 </head>
