2fb5b03670bcc6efe3a06bb7d88208d3d1535576
Sebastian Herrmann <sebastian@herrherrmann.net>
2021-08-13 14:36:34 +0200

    Update download-monitor templates for new version of plugin

--- download-monitor/content-download-document.php
+++ download-monitor/content-download-document.php
@@ -1,6 +1,16 @@
-<?php global $dlm_download; ?>
+<?php
+/**
+ * Download Document
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // Exit if accessed directly
+
+/** @var DLM_Download $dlm_download */
+?>
 <div class="download-document">
-	<a class="download-link" title="Download (<?php $dlm_download->the_filesize(); ?>)" href="<?php $dlm_download->the_download_link(); ?>" rel="nofollow">
+	<a class="download-link" href="<?php $dlm_download->the_download_link(); ?>" title="Download (<?php echo $dlm_download->get_version()->get_filesize_formatted(); ?>)" rel="nofollow">
 		<?php $dlm_download->the_title(); ?>
 	</a>
 	<div class="download-description">
--- download-monitor/content-download-musicalbum.php
+++ download-monitor/content-download-musicalbum.php
@@ -1,4 +1,14 @@
-<?php global $dlm_download; ?>
+<?php
+/**
+ * Download Music Album
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // Exit if accessed directly
+
+/** @var DLM_Download $dlm_download */
+?>
 <div class="download-musicalbum">
 	<a class="cover" href="<?php $dlm_download->the_download_link(); ?>" title="Download (<?php $dlm_download->the_filesize(); ?>)" rel="nofollow">
 		<?php $dlm_download->the_image('thumbnail'); ?>
@@ -12,7 +22,7 @@
 				<?php $dlm_download->the_short_description(); ?>
 			</div>
 		</div>
-		<a class="download-link" href="<?php $dlm_download->the_download_link(); ?>" title="Download (<?php $dlm_download->the_filesize(); ?>)" rel="nofollow">
+		<a class="download-link" href="<?php $dlm_download->the_download_link(); ?>" title="Download (<?php echo $dlm_download->get_version()->get_filesize_formatted(); ?>)" rel="nofollow">
 			Download
 		</a>
 	</div>
