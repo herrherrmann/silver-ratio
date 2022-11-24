8a2e56244895d6ab87d86835d36184fd818ae193
Sebastian Herrmann <sebastian@herrherrmann.net>
2022-11-24 11:11:57 +0100

    Consistently use tabs for indentation

--- footer.php
+++ footer.php
@@ -1,16 +1,16 @@
-                </div>
-                <footer class="footer">
-                    <nav class="menu-footer" aria-label="Footer Menu">
-                        <?php wp_nav_menu( array(
-                            'theme_location' => 'footer-menu',
-                            'container'      => false
-                        )); ?>
-                    </nav>
-                </footer>
+				</div>
+				<footer class="footer">
+					<nav class="menu-footer" aria-label="Footer Menu">
+						<?php wp_nav_menu( array(
+							'theme_location' => 'footer-menu',
+							'container'      => false
+						)); ?>
+					</nav>
+				</footer>
 			</main>
 		</div>
-    </div>
-    <?php wp_footer(); ?>
+	</div>
+	<?php wp_footer(); ?>
 </body>
 
 </html>
