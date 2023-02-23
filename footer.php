e28f406a1a7adb4a0f397afc9bb45f09a261e035
Sebastian Herrmann <sebastian@herrherrmann.net>
2023-02-23 13:47:19 +0100

    Change footer style to have dark background and be below all other main elements

--- footer.php
+++ footer.php
@@ -1,15 +1,15 @@
 				</div>
-				<footer class="footer">
-					<nav class="menu-footer" aria-label="Footer Menu">
-						<?php wp_nav_menu( array(
-							'theme_location' => 'footer-menu',
-							'container'      => false
-						)); ?>
-					</nav>
-				</footer>
 			</main>
 		</div>
 	</div>
+	<footer class="footer">
+		<nav class="menu-footer" aria-label="Footer Menu">
+			<?php wp_nav_menu( array(
+				'theme_location' => 'footer-menu',
+				'container'      => false
+			)); ?>
+		</nav>
+	</footer>
 	<?php wp_footer(); ?>
 </body>
 
